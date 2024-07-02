3af08fec0813ad11fcf76d3f116d76510e5aeca8
groza_1337 <groza_1337@mail.ru>
2024-07-02 16:52:24 +0300

    Сделал тестовую страничку. Если при заходе пишется Hello Sergey - значит все работает

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,33 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import './App.css'
 const URL_API = import.meta.env.DEV ? 'http://localhost:8000/api' : '/api';
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState('');
 
+  useEffect(() => {
+
+    // Функция для получения данных с бэкенда
+    const fetchMessage = async () => {
+      try {
+        const response = await fetch(`${URL_API}/hello/Sergey`);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        setMessage(data.message);
+      } catch (error) {
+        console.error('Ошибка при получении данных:', error);
+      }
+    };
+
+    fetchMessage();
+  }, []);
   return (
-    <>
-
-    </>
+    <h1>
+      {message}
+    </h1>
   )
 }
 

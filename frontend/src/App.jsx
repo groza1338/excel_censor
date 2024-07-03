248afef10a8eb0534453d165a2ebaf780851065b
groza_1337 <groza_1337@mail.ru>
2024-07-03 23:35:46 +0300

    Сделал просто Hello World на фронте

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,29 +4,10 @@ const URL_API = import.meta.env.DEV ? 'http://localhost:8000/api' : '/api';
 
 
 function App() {
-  const [message, setMessage] = useState('');
 
-  useEffect(() => {
-
-    // Функция для получения данных с бэкенда
-    const fetchMessage = async () => {
-      try {
-        const response = await fetch(`${URL_API}/hello/Sergey`);
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.json();
-        setMessage(data.message);
-      } catch (error) {
-        console.error('Ошибка при получении данных:', error);
-      }
-    };
-
-    fetchMessage();
-  }, []);
   return (
     <h1>
-      {message}
+      Hello, world!
     </h1>
   )
 }

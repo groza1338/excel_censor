26d77bfea291bc7a4563072f08b453b1631d97e9
groza_1337 <groza_1337@mail.ru>
2024-07-09 20:06:20 +0300

    Теперь, если много колонок, они помещаются в scroll-area

--- frontend/src/components/ColumnSelector.jsx
+++ frontend/src/components/ColumnSelector.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
-import { Checkbox, List } from 'antd';
+import React, { useEffect, useRef } from 'react';
+import { Checkbox } from 'antd';
+import './ColumnSelector.css';
 
 const ColumnSelector = ({ columns, selectedColumns, setSelectedColumns }) => {
+  const scrollAreaRef = useRef(null);
+
+  useEffect(() => {
+    const scrollArea = scrollAreaRef.current;
+    if (scrollArea.scrollHeight > scrollArea.clientHeight) {
+      scrollArea.classList.add('has-scroll');
+    } else {
+      scrollArea.classList.remove('has-scroll');
+    }
+  }, [columns]);
+
   const onChange = (checkedValues) => {
     setSelectedColumns(checkedValues);
   };
@@ -10,14 +22,13 @@ const ColumnSelector = ({ columns, selectedColumns, setSelectedColumns }) => {
     <div>
       <h3>Выберете колонки, которые хотите спрятать</h3>
       <Checkbox.Group onChange={onChange} value={selectedColumns}>
-        <List
-          dataSource={columns}
-          renderItem={(item, index) => (
-            <List.Item>
+        <div className="scroll-area" ref={scrollAreaRef}>
+          {columns.map((item, index) => (
+            <div className="column-selector-item" key={index}>
               <Checkbox value={index}>{item}</Checkbox>
-            </List.Item>
-          )}
-        />
+            </div>
+          ))}
+        </div>
       </Checkbox.Group>
     </div>
   );
